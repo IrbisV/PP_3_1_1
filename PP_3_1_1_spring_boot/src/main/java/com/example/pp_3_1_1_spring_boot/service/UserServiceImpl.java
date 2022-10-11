652e016d8f31f102a642be712fa158b263311334
Dmitry Vanyukov <ekoetnotur@yandex.ru>
2022-10-11 17:40:15 +0300

    implements UserService

---
+++ PP_3_1_1_spring_boot/src/main/java/com/example/pp_3_1_1_spring_boot/service/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package com.example.pp_3_1_1_spring_boot.service;
+
+import com.example.pp_3_1_1_spring_boot.model.User;
+import com.example.pp_3_1_1_spring_boot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+@Component
+@Transactional
+public class UserServiceImpl implements UserService{
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.getById(id);
+    }
+
+    @Override
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public void removeUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
